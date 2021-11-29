0c525be7f701e6cb7ecb4ebfe6c326c0998f8d1b
Daniel Kim <bungfoon@gmail.com>
2021-11-29 15:29:36 -0500

    adds Navbar component with styled-components imported

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Home from './components/pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <Home />
+    );
 }
 
 export default App;
