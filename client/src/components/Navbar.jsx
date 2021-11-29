bd8d3999b1901d3c747706f664f913740de572f0
Daniel Kim <bungfoon@gmail.com>
2021-11-29 16:03:22 -0500

    adds initial styling for Navbar on Homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``
+const Container = styled.div`
+    height: 60px;
+    background-color: gray;
+`
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Left = styled.div``
+const Center = styled.div``
+const Right = styled.div``
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>left</Left>
+                <Center>center</Center>
+                <Right>right</Right>
+            </Wrapper>
+        </Container>
     );
 };
 
