54fa8222dae69802362b4ecf90fd554c37185e15
Daniel Kim <bungfoon@gmail.com>
2021-11-29 21:52:12 -0500

    adds correctly positioned slider arrows on homepage

---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,41 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    height: 100vh;
+    background-color: coral;
+    position: relative;
+`;
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff7f7;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+    margin: auto;
+`;
+
+const Slider = () => {
+    return (
+        <Container>
+            <Arrow direction="left">
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Arrow direction="right">
+                <ArrowRightOutlined />
+            </Arrow>
+        </Container>
+    );
+};
+
+export default Slider;
